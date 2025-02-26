5c23dfae2091e5911da45e0f90030c73d3e224fe
jlescano-mobup <jlescano@mobuptacna.com>
2025-02-26 12:45:26 -0500

    Manipulando contenido HTML con JS, uso de document.querySelector

---
+++ 2034-logica-programacion-2/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del número secreto'; // Cambia el contenido de h1 por 'Juego del número secreto'
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Ingresa un número entre 1 y 100'; // Cambia el contenido de p por 'Ingresa un número entre 1 y 100'
