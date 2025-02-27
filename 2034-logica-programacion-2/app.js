c7920763efecd2247b2380a65d17bff81821eb37
jlescano-mobup <jlescano@mobuptacna.com>
2025-02-27 12:21:51 -0500

    en index.html se agrega boton y en otro boton se inserta una funcion que genera un alert

--- 2034-logica-programacion-2/app.js
+++ 2034-logica-programacion-2/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Juego del número secreto'; // Cambia el contenido de h1 por
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Ingresa un número entre 1 y 100'; // Cambia el contenido de p por 'Ingresa un número entre 1 y 100'
+
+function intentoDeSuario() {
+    alert('Intento click de usuario');
+}
