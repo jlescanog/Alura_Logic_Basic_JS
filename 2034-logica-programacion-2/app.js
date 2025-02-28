83fcfaac77311f47746c2443bd647cd5fa6fbcca
jlescano-mobup <jlescano@mobuptacna.com>
2025-02-28 15:39:48 -0500

    se agrega funcion para cambiar textos, se crea logica para numero aleatorio y se recibe numero por imput

--- 2034-logica-programacion-2/app.js
+++ 2034-logica-programacion-2/app.js
@@ -1,9 +1,25 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto'; // Cambia el contenido de h1 por 'Juego del número secreto'
+let numeroSecreto = generarNumeroSecreto();
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Ingresa un número entre 1 y 100'; // Cambia el contenido de p por 'Ingresa un número entre 1 y 100'
-
-function intentoDeSuario() {
-    alert('Intento click de usuario');
+// Crea una función asignarTextoElemento que reciba dos parámetros: un elemento y un texto. La función debe asignar el texto recibido al elemento recibido.
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario == numeroSecreto);
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+asignarTextoElemento('h1', 'Juego del número secreto!'); // Cambia el contenido de h1 por 'Juego del número secreto!'
+asignarTextoElemento('p', 'Ingresa un número del 1 al 10') // Cambia el contenido de p por 'Bienvenido a mi sitio'
